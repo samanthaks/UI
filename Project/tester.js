bdb39203caeb5e96584ff338d31aef5c2235ed0a
Sam Stultz <sks2200@columbia.edu>
2018-04-29 16:44:01 -0400

    updates

--- Project/tester.js
+++ Project/tester.js
@@ -1,121 +1,138 @@
 videos = [
     {
-      "name": "Sheet Pan Quinoa Pizza Crust",
+      "name": "Gyoza Dumplings",
       "times": [
-          1,3,
-          4,10, 
-          11, 13,
-          14, 28,
-          29, 38,
-          39, 44,
-          45, 62,
-          63, 83
+          59, 61,
+          62, 69, 
+          70, 73,
+          74, 85,
+          86, 87,
+          88, 91,
+          92, 93,
+          94, 98,
+          99, 103
         ],
-      "id": "F0RceeajFzk"
+      "id": "8JXyDtsVGQ0",
+      "img": "gyoza1.jpg",
+      "steps": [
+        {
+          "name": "Step 1",
+          "description": "Put pork, sake, salt, and sugar in a bowl. Mix until meat is sticky."
+        },
+        {
+          "name": "Step 2",
+          "description": "Combine garlic, ginger, soy sauce, sesame oil, potato starch, cabbage, and chive. Mix them very well in a bowl."
+        },
+        {
+          "name": "Step 3",
+          "description": "Scoop a spoonful of filling onto the middle of the gyoza skin. Apply water for skin edge with your finger."
+        },
+        {
+          "name": "Step 4",
+          "description": "Fold in half and pinch pleats into the edges, then wrap up."
+        },
+        {
+          "name": "Step 5",
+          "description": "Heat sesame oil in the pan and lay gyoza in single layer. Keep on medium heat for 2 minutes."
+        },
+        {
+          "name": "Step 6",
+          "description": "When gyoza turns brown, pour in slurry and put the lid on. Steam for 3 minutes."
+        },
+        {
+          "name": "Step 7",
+          "description": "Take off the lid, put more sesame oil and cook over low heat."
+        },
+        {
+          "name": "Step 8",
+          "description": "Cover a pan with a plate, turn over a pan."
+        },
+        {
+          "name": "Step 9",
+          "description": "Enjoy!"
+        }]
     },
     {
-      "name": "Energy-Boosting Buddha Bowl",
+      "name": "Rose Dumplings",
       "times": [
-          1,5,
-          6,15, 
-          16, 35,
-          36, 38,
-          39, 56,
-          57, 91
+          59, 61,
+          62, 69, 
+          70, 73,
+          74, 85,
+          86, 87,
+          88, 91,
+          92, 93,
+          94, 98,
+          99, 103
         ],
-      "id": "dbGUYwebQmE"
+      "id": "8JXyDtsVGQ0", // after gyoza dumpling
+      "img": "rosedumpling.png"
     },
+    // {
+    //   "name": "Vegetarian Potstickers",
+    //   "times": [
+    //       59, 61,
+    //       62, 69, 
+    //       70, 73,
+    //       74, 85,
+    //       86, 87,
+    //       88, 91,
+    //       92, 93,
+    //       94, 98,
+    //       99, 103
+    //     ],
+    //   "id": "K2UieyGTHOw",
+    //   "img": "vegpotsticker.png"
+    // },
     {
-      "name": "Cucumber, Tomato, and Avocado Salad",
+      "name": "Pan-Fried Soup Dumplings",
       "times": [
-          1,6,
-          7,12, 
-          13, 18,
-          19, 25,
-          26, 29,
-          30, 44
+          59, 61,
+          62, 69, 
+          70, 73,
+          74, 85,
+          86, 87,
+          88, 91,
+          92, 93,
+          94, 98,
+          99, 103
         ],
-      "id": "vUBpiP9aPtg"
+      "id": "pEdMpRuFGEk", 
+      "img": "panfriedsoupdumpling.png"
     }
   ];
 
 
-var faves = [];
+
 var step_list = [];
 var count = 0;
-var speed = 1;
 var vid_array_idx = 0;
+var loop_on = false;
 
 $(document).ready(function(){
   loadScript();
 
   $('#home-nav').addClass('active');
-  $('#vid-body').hide();
-  $('#vid-faves-body').hide();
-  $('#remove-fave-button').hide();
-  $('#remove-fave').hide();
+  $('#vid-area').hide();
   $('#watch-nav').hide();
 
   $('#watch-nav').click(function(){
     $('#watch-nav').addClass('active');
-    $('#fave-nav').removeClass('active');
     $('#home-nav').removeClass('active');
 
     $('#vid-menu-body').hide();
-    $('#vid-body').show();
-    $('#vid-faves-body').hide();
-  })
-  $('#fave-nav').click(function(){
-    $('#watch-nav').removeClass('active');
-    $('#fave-nav').addClass('active');
-    $('#home-nav').removeClass('active');
-
-    $('#vid-menu-body').hide();
-    $('#vid-body').hide();
-    $('#vid-faves-body').show();
+    $('#vid-area').show();
   })
   $('#home-nav').click(function(){
     $('#watch-nav').removeClass('active');
-    $('#fave-nav').removeClass('active');
     $('#home-nav').addClass('active');
 
     $('#vid-menu-body').show();
-    $('#vid-body').hide();
-    $('#vid-faves-body').hide();
-  })
-
-  $('#add-fave-button').click(function(){
-    $('#add-fave-button').hide();
-    $('#add-fave').hide();
-
-    if (faves[vid_array_idx]){
-      faves[vid_array_idx].push(count);
-    }
-    else {
-      faves[vid_array_idx] = [];
-      faves[vid_array_idx].push(count);
-    }
-    createFavesUI();
-
-    $('#remove-fave-button').show();
-    $('#remove-fave').show();
-  })
-  $('#remove-fave-button').click(function(){
-    $('#add-fave-button').show();
-    $('#add-fave').show();
-
-    var index = faves[vid_array_idx].indexOf(count);
-    faves[vid_array_idx].splice(index, 1);
-    
-    createFavesUI();
-
-    $('#remove-fave-button').hide();
-    $('#remove-fave').hide();
+    $('#vid-area').hide();
   })
 
   createHomeUI();
   
-
 })
 
 function loadScript() {
@@ -136,7 +153,7 @@ var ytplayer;
 function onPlayerReady(event) {
   event.target.playVideo();
   ytplayer.setVolume(0);
-  repeatInt = setInterval('loop()', 1000);
+  repeatInt = setInterval('play_steps()', 1000);
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -145,7 +162,7 @@ function onPlayerReady(event) {
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.ENDED) {
     ytplayer.seekTo(parseInt(4), true);
-    ytplayer.playVideo()
+    ytplayer.playVideo();
   }
 }
 
@@ -155,33 +172,48 @@ function stopVideo() {
 
 function pauseVideo(){
   ytplayer.pauseVideo();
+  $('#play').show();
+  $('#play-button').show();
+  $('#pause').hide();
+  $('#pause-button').hide();
 }
 
 function playVideo(){
   ytplayer.playVideo();
+  $('#pause').show();
+  $('#pause-button').show();
+  $('#play').hide();
+  $('#play-button').hide();
 }
 
-function setPlaybackRate(count){
-  console.log(count);
-  console.log(ytplayer.getAvailablePlaybackRates());
-  ytplayer.setPlaybackRate(count);
+function setPlaybackRate(speed){
+  ytplayer.setPlaybackRate(speed);
 }
 
-function loop() {
-  if (parseInt(videos[vid_array_idx]['times'][count+1]) < ytplayer.getCurrentTime()) {
-  	console.log("LOOP");
-  	console.log(ytplayer.getCurrentTime());
+function play_steps() {
+  if (loop_on){
+    if (parseInt(videos[vid_array_idx]['times'][count+1]) < ytplayer.getCurrentTime()) {
+    	console.log("LOOP");
+    	console.log(ytplayer.getCurrentTime());
       ytplayer.seekTo(parseInt(videos[vid_array_idx]['times'][count]), true);
+    }
+    if (parseInt(videos[vid_array_idx]['times'][count]) > ytplayer.getCurrentTime()) {
+      ytplayer.seekTo(videos[vid_array_idx]['times'][count], true);
+    }
   }
-  if (parseInt(videos[vid_array_idx]['times'][count]) > ytplayer.getCurrentTime()) {
-    ytplayer.seekTo(videos[vid_array_idx]['times'][count], true);
+  else {
+    if (parseInt(videos[vid_array_idx]['times'][count+1]) < ytplayer.getCurrentTime()) {
+      ytplayer.seekTo(parseInt(videos[vid_array_idx]['times'][count]), true);
+      pauseVideo();
+    }
+    if (parseInt(videos[vid_array_idx]['times'][count]) > ytplayer.getCurrentTime()) {
+      ytplayer.seekTo(videos[vid_array_idx]['times'][count], true);
+    }
   }
 }
 
 function createVidUI(vid_index) {
   // 2. This code loads the IFrame Player API code asynchronously.
-  $('#play-prev').hide();
-  $('#play-prev-button').hide();
   $('#play').hide();
   $('#play-button').hide();
 
@@ -193,7 +225,7 @@ function createVidUI(vid_index) {
   console.log(vid_id);
   ytplayer = new YT.Player('player', {
     height: '390',
-    width: '640',
+    width: '100%',
     playerVars: { 'autoplay': 1, 'loop': 1, 'rel': 0, 'fs': 1, 
     'modestbranding' : 1, 'iv_load_policy': 3, 'controls': 0 },
 
@@ -209,177 +241,99 @@ function createVidUI(vid_index) {
     $('#play-prev').show();
     $('#play-prev-button').show();
     count = count + 2;
-    $('#step-title').html("Step ".concat(parseInt((count/2)+1)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
-    loop();
+    $('#step-num').html("Step ".concat(parseInt((count/2)+1)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
+    $('#step-title').html(videos[parseInt(vid_index)]["steps"][parseInt((count/2)+1)-1]["description"]);
+
+    ytplayer.seekTo(parseInt(videos[vid_array_idx]['times'][count]), true);
+    playVideo();
+
     if (count == videos[vid_array_idx]['times'].length - 2){
       $('#play-next').hide();
       $('#play-next-button').hide();
-    }
-    if (faves[vid_array_idx]){
-      if (faves[vid_array_idx].indexOf(count) != -1){
-        $('#add-fave-button').hide();
-        $('#add-fave').hide();
-        $('#remove-fave-button').show();
-        $('#remove-fave').show();
-      }
-      else {
-        $('#add-fave-button').show();
-        $('#add-fave').show();
-        $('#remove-fave-button').hide();
-        $('#remove-fave').hide();
-      }
-    } else {
-      $('#add-fave-button').show();
-      $('#add-fave').show();
-      $('#remove-fave-button').hide();
-      $('#remove-fave').hide();
+      $('#finished').show();
     }
   })
 
   $('#play-prev-button').click(function(){
     $('#play-next').show();
     $('#play-next-button').show();
-    count = count - 2;
-    $('#step-title').html("Step ".concat(parseInt((count/2)+1)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
-    loop();
-    if (count == 0){
-      $('#play-prev').hide();
-      $('#play-prev-button').hide();
+    $('#finished').hide();
+    if (count != 0){
+      count = count - 2;
+      $('#step-num').html("Step ".concat(parseInt((count/2)+1)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
+      $('#step-title').html(videos[parseInt(vid_index)]["steps"][parseInt((count/2)+1)-1]["description"]);
+      ytplayer.seekTo(parseInt(videos[vid_array_idx]['times'][count]), true);
+      playVideo();
     }
-    if (faves[vid_array_idx]){
-      if (faves[vid_array_idx].indexOf(count) != -1){
-        $('#add-fave-button').hide();
-        $('#add-fave').hide();
-        $('#remove-fave-button').show();
-        $('#remove-fave').show();
-      }
-      else {
-        $('#add-fave-button').show();
-        $('#add-fave').show();
-        $('#remove-fave-button').hide();
-        $('#remove-fave').hide();
-      }
-    } else {
-      $('#add-fave-button').show();
-      $('#add-fave').show();
-      $('#remove-fave-button').hide();
-      $('#remove-fave').hide();
+    else {
+      ytplayer.seekTo(parseInt(videos[vid_array_idx]['times'][count]), true);
+      playVideo();
     }
   })
 
-  $('#skip-step-drop').on('change', function () {
-    console.log("SKIP STEP");
-    var num = $(this).val(); // get selected value
 
-    count = (num - 1)*2;
-    $('#step-title').html("Step ".concat(parseInt(num)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
-    $('#play-next').show();
-    $('#play-next-button').show();
-    $('#play-prev').show();
-    $('#play-prev-button').show();
-    if (count == videos[vid_array_idx]['times'].length - 2){
-      $('#play-next').hide();
-      $('#play-next-button').hide();
-    }
-    if (count == 0){
-      $('#play-prev').hide();
-      $('#play-prev-button').hide();
-    }
-    $('#skip-step-drop').val(0);
-    if (faves[vid_array_idx]){
-      if (faves[vid_array_idx].indexOf(count) != -1){
-        $('#add-fave-button').hide();
-        $('#add-fave').hide();
-        $('#remove-fave-button').show();
-        $('#remove-fave').show();
-      }
-      else {
-        $('#add-fave-button').show();
-        $('#add-fave').show();
-        $('#remove-fave-button').hide();
-        $('#remove-fave').hide();
-      }
-    } else {
-      $('#add-fave-button').show();
-      $('#add-fave').show();
-      $('#remove-fave-button').hide();
-      $('#remove-fave').hide();
-    }
-    loop();
-  
+  $('#speed-list a').click(function(e) {
+    $('#speed-list a.active').removeClass('active');
+    var speed = $(this).text();
+    speed = parseFloat(speed.substring(0, speed.length-1));
+    $(this).addClass('active');
+    console.log("set speed to ", speed);
+    e.stopPropagation();
+    setPlaybackRate(speed);
   });
+
 
   $('#pause-button').click(function(){
     pauseVideo();
-    $('#play').show();
-    $('#play-button').show();
-    $('#pause').hide();
-    $('#pause-button').hide();
   })
 
   $('#play-button').click(function(){
     playVideo();
-    $('#pause').show();
-    $('#pause-button').show();
-    $('#play').hide();
-    $('#play-button').hide();
   })
 
-  $('#play-slower-button').click(function(){
-    speed -= 0.25;
-    setPlaybackRate(speed);
+  $('#finished').click(function(){
+    alert("Congrats you've learnt how to make " + videos[vid_array_idx]["name"] + "!! Watch more videos to learn more recipes!");
+    $('#watch-nav').removeClass('active');
+    $('#home-nav').addClass('active');
+
+    $('#vid-menu-body').show();
+    $('#vid-area').hide();
   })
 
-  $('#play-faster-button').click(function(){
-    speed += 0.25;
-    setPlaybackRate(speed);
+  $('#unloop-button').click(function(){
+    console.log('unloop');
+    $('#loop-button').show();
+    $('#unloop-button').hide();
+    loop_on = false;
   })
+
+  $('#loop-button').click(function(){
+    console.log('loop');
+    $('#loop-button').hide();
+    $('#unloop-button').show();
+    loop_on = true;
+  })
+
 }
 
-
-function createFavesUI() {
-  console.log("making list");
-  $('#vid-faves-body').empty();
-  console.log(faves);
-  for (var key in faves) {
-    console.log(key);
-    var new_vid_fave = '<div class="row"><div class="col-md-6 offset-md-3"><div class="row fave-title">'
-      + '<h6>' + videos[parseInt(key)]['name'] + '</h6>'
-      + '</div>';
-    faves[key].sort();
-    faves[key].forEach ((element, index, array) => {
-      step = (element/2)+1;
-      new_vid_fave += '<div class="row menu-item-row">'
-      + '<button type="button" class="btn btn-light watch-fave-vid" id="'+element+'"" value="'+ parseInt(key) +'">Step '
-      + step + '</button>'
-      + '</div>';
-      console.log(element);
-    });
-    new_vid_fave += '</div></div>';
-    $('#vid-faves-body').append(new_vid_fave);
-  }
-
-  $('.watch-fave-vid').on("click", function(event){
-    console.log("fave video clicked");
-    vid_array_idx = parseInt(event['target']['value']);
-    ytplayer.loadVideoById(videos[vid_array_idx]["id"]);
-
-    count = parseInt(event['target']['id']);
-    loadVideoPage();
-  });
-  
-}
 
 
 function createHomeUI() {
-  videos.forEach((element, index, array) => {
-    var new_row = $('<div class="row menu-item-row">' 
-      + '<div class="col-md-6 offset-md-3">'
-      + '<button type="button" class="btn btn-light watch-vid" value="'+index+'">'
-      + element.name + '</button></div>'
-      + '</div>');
-    $("#vid-menu-body").append(new_row);
-  });
+  // videos.forEach((element, index, array) => {
+  //   var new_row = $('<div class="row menu-item-row">' 
+  //     + '<div class="col-md-6 offset-md-3">'
+  //     + '<button type="button" class="btn btn-light watch-vid" value="'+index+'">'
+  //     + element.name + '</button></div>'
+  //     + '</div>');
+  //   $("#vid-menu-body").append(new_row);
+  // });
+
+  var new_row = $('<div class="row menu-item-row">' 
+    + '<div class="col-md-6 offset-md-3">'
+    + '<button type="button" class="btn btn-light watch-vid" value="0">'
+    + videos[0].name + '</button></div>'
+    + '</div>');
+  $("#vid-menu-body").append(new_row);
 
   $('.watch-vid').on("click", function(event){
     console.log("video clicked");
@@ -392,8 +346,6 @@ function createHomeUI() {
     }
 
     count = 0;
-    $('#play-prev').hide();
-    $('#play-prev-button').hide();
     $('#play-next').show();
     $('#play-next-button').show();
     loadVideoPage();
@@ -402,50 +354,68 @@ function createHomeUI() {
 }
   
 function loadVideoPage() {
+  $('#finished').hide();
   step_list = [];
   for (var i = 0; i <= videos[vid_array_idx]["times"].length - 2; i = i + 2) {
     step_list.push(i/2 + 1);
   }
 
-  var option = '';
-  $('#skip-step-drop').empty();
-  for (var i=0;i<step_list.length;i++){
-     option += '<option value="'+ step_list[i] + '">' + step_list[i] + '</option>';
-  }
-  $('#skip-step-drop').append(option);
-  $('#skip-step-drop').val(0);
+  console.log(step_list);
 
-  $('#recipe-title').html(videos[vid_array_idx]["name"]);
-  $('#step-title').html("Step ".concat(parseInt((count/2)+1)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
+  $('#unloop-button').hide();
+
+
+  $('#recipe-icon').html('<img src="'.concat(videos[vid_array_idx]["img"]).concat('" id="vid-icon">'));
+  $('#recipe-title').html(videos[vid_array_idx]["name"])
+  $('#step-num').html("Step ".concat(parseInt((count/2)+1)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
+  $('#step-title').html(videos[vid_array_idx]["steps"][parseInt((count/2)+1)-1]["description"]);
+  // $('#recipe-title').html(videos[vid_array_idx]["name"]);
 
   $('#watch-nav').show();
   $('#watch-nav').addClass('active');
-  $('#fave-nav').removeClass('active');
   $('#home-nav').removeClass('active');
 
   $('#vid-menu-body').hide();
-  $('#vid-body').show();
-  $('#vid-faves-body').hide();
+  $('#vid-area').show();
 
-  if (faves[vid_array_idx]){
-    if (faves[vid_array_idx].indexOf(count) != -1){
-      $('#add-fave-button').hide();
-      $('#add-fave').hide();
-      $('#remove-fave-button').show();
-      $('#remove-fave').show();
+  videos[vid_array_idx]["steps"].forEach((element, index, array) => {
+    console.log(index+1);
+    var new_row = $('<div class="playlist-item dropdown-item" value="'+ (index+1) +'"><strong>'
+      + element['name'] + ':</strong> ' + videos[vid_array_idx]["steps"][index]["description"]
+      + '</div>');
+    if (index == 0){
+      new_row.addClass('active');
     }
-    else {
-      $('#add-fave-button').show();
-      $('#add-fave').show();
-      $('#remove-fave-button').hide();
-      $('#remove-fave').hide();
+    $("#playlist-body").append(new_row);
+
+  });
+
+  $('#playlist-body div').click(function(e) {
+    console.log('hello');
+    $('#playlist-body div.active').removeClass('active');
+    $(this).addClass('active');
+
+    var num = parseInt(e['target']['attributes']['value']['nodeValue']);
+    count = (num - 1)*2;
+    $('#step-num').html("Step ".concat(parseInt(num)).concat(" of ").concat(parseInt(videos[vid_array_idx]['times'].length/2)));
+    $('#step-title').html(videos[vid_array_idx]["steps"][parseInt((count/2)+1)-1]["description"]);
+
+    ytplayer.seekTo(parseInt(videos[vid_array_idx]['times'][count]), true);
+    if (count == videos[vid_array_idx]['times'].length - 2){
+      $('#play-next').hide();
+      $('#play-next-button').hide();
+      $('#finished').show();
     }
-  } else {
-    $('#add-fave-button').show();
-    $('#add-fave').show();
-    $('#remove-fave-button').hide();
-    $('#remove-fave').hide();
-  }
+    else{
+      $('#play-next').show();
+      $('#play-next-button').show();
+      $('#finished').hide();
+    }
+    playVideo();
+
+    e.stopPropagation();
+  });
+
 }
 
 
